d2a5c2f63c26413e20b1592d6cf3526693225c8a
unknown <Chanthavone@Chanthavone-PC.(none)>
2012-04-18 19:48:21 +0200

    Rassemblement des identifications

---
+++ site_client/identification.php
@@ -0,0 +1,9 @@
+﻿<h1>Indentification C</h1>
+
+<form action="index.php?module=connexion" method="POST">
+	<fieldset>
+		<label>Nom de l'abonné : </label><input type="text" name="nom" value="Belmi"/><br />
+		<label>Numéro de l'abonné : </label><input type="text" name="pass" value="4367Xs" /><br />
+		<input type="submit" value="Envoyer" />
+	</fieldset>
+</form>
---
+++ site_client/menuClient.php
@@ -0,0 +1,4 @@
+﻿<ul id="menuClient">
+	<li> <a href="index.php?module=detenues">Videos détenues</a> </li>
+	<li> <a href="index.php?module=commande">Vos commandes</a> </li>
+</ul>

40bb830b2f52a8fe91b3a1f69930c7b38428257e
unknown <Abdoul@Abdoul-PC.(none)>
2012-04-23 17:42:07 +0200

    Amelioration panier

--- site_client/descriptif.php
+++ site_client/descriptif.php
@@ -8,44 +8,52 @@
 	include("modele/modeleActeurs.php");
 	
 	//on verifie si les elements du formulaire existe
-	if(isset($_POST['numFilm']) && preg_match("#[0-9]+$#",$_POST['numFilm'])){
+	if(isset($_POST['numFilm']) || isset($_GET['numFilm'])){
 	
 		//on initialise la variable
-		$numFilm = $_POST['numFilm'] ;
-		//on appelle la fonction qui permet de trouver un film selon son numero
-		$film = getFilm($numFilm);
-		
-		//Si on a trouve le film, on affiche ces caracteristiques
-		if(count($film) > 1){
-			echo '<table border="1">';
-				echo '<tr><td>Numéro du film </td><td>'.$film["NoFilm"].'</td></tr>
-					<tr><td>Titre </td><td>'.$film["Titre"].'</td></tr>
-					<tr><td>Nationalité </td><td>'.$film["Nationalite"].'</td></tr>
-					<tr><td>Réalisateur </td><td>'.$film["Realisateur"].'</td></tr>
-					<tr><td>Année de production </td><td>'.$film["Annee"].'</td></tr>
-					<tr><td>Couleur </td><td>'.$film["Couleur"].'</td></tr>
-					<tr><td>Durée </td><td>'.$film["Duree"].'</td></tr>
-					<tr><td>Résumé </td><td>'.$film["Synopsis"].'</td></tr>
-					<tr><td>Genre </td><td>'.$film["Genre"].'</td></tr>
-					<tr><td>Liste des principaux acteurs </td><td>'.getActeursFilm($numFilm).'</td></tr>
-				';
-			echo '</table>';
-			//Ajout selection
-			echo '<form action="index.php?module=ajoutSelection" method="post">
-				<input type="hidden" name="noFilm" value="'.$film["NoFilm"].'" />
-				<input type="submit" value="Ajout Selection" />
-				</form>';
-			//Voir selection	
-			echo '<form action="index.php?module=voirSelection" method="post">
-				<input type="submit" value="Voir Selection" />
-				</form>';
+		if(isset($_POST['numFilm']))
+			$numFilm = $_POST['numFilm'] ;
+		else
+			$numFilm = $_GET['numFilm'];
+		if(preg_match("#[0-9]+$#",$numFilm)){
+			//on appelle la fonction qui permet de trouver un film selon son numero
+			$film = getFilm($numFilm);
+			
+			//Si on a trouve le film, on affiche ces caracteristiques
+			if(count($film) > 1){
+				echo '<table border="1">';
+					echo '<tr><td>Numéro du film </td><td>'.$film["NoFilm"].'</td></tr>
+						<tr><td>Titre </td><td>'.$film["Titre"].'</td></tr>
+						<tr><td>Nationalité </td><td>'.$film["Nationalite"].'</td></tr>
+						<tr><td>Réalisateur </td><td>'.$film["Realisateur"].'</td></tr>
+						<tr><td>Année de production </td><td>'.$film["Annee"].'</td></tr>
+						<tr><td>Couleur </td><td>'.$film["Couleur"].'</td></tr>
+						<tr><td>Durée </td><td>'.$film["Duree"].'</td></tr>
+						<tr><td>Résumé </td><td>'.$film["Synopsis"].'</td></tr>
+						<tr><td>Genre </td><td>'.$film["Genre"].'</td></tr>
+						<tr><td>Liste des principaux acteurs </td><td>'.getActeursFilm($numFilm).'</td></tr>
+					';
+				echo '</table>';
+				//Ajout selection
+				echo '<form action="index.php?module=ajoutSelection" method="post">
+					<input type="hidden" name="noFilm" value="'.$film["NoFilm"].'" />
+					<input type="submit" value="Ajout Selection" />
+					</form>';
+				//Voir selection	
+				echo '<form action="index.php?module=voirSelection" method="post">
+					<input type="submit" value="Voir Selection" />
+					</form>';
+			}
+			else{
+				echo '<h1>Le numéro '.$numFilm.' n\'existe pas !</h1>';
+			}
 		}
 		else{
-			echo '<h1>Le numéro '.$numFilm.' n\'existe pas !</h1>';
+			echo "<h3>Numéro de film incorrect !</h3>";
 		}
 	}
 	else{
-		echo "<h3>Numéro de film incorrect !</h3>";
+		echo "<h3>Impossible d'avoir le descriptif !</h3>";
 	}
 	//Lien de retour
 	echo '<a href="index.php?module=accueilDescriptif">Retour</a>';

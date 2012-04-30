be28aa6aca417627b022e76e304333d6170ab71c
Chanthavone <cedricchanthavone@hotmail.fr>
2012-04-30 15:02:10 +0200

    Amélioration du style et ajout du lien sur les titre de recherche

--- site_client/identificationC.php
+++ site_client/identificationC.php
@@ -3,9 +3,10 @@
 </div>
 
 <form action="index.php?module=commande" method="POST">
-	<fieldset>
-		<label>Nom de l'abonné : </label><input type="text" name="nom" value="Belmi"/><br />
-		<label>Numéro de l'abonné : </label><input type="text" name="pass" value="4367Xs" /><br />
-		<input type="submit" value="Envoyer" />
+	<fieldset class="formulaire">
+        <legend>Identifiez vous</legend> <br />
+		<label class="label_recherche">Nom de l'abonné : </label><input type="text" name="nom" value="Belmi"/><br />
+		<label class="label_recherche">Numéro de l'abonné : </label><input type="text" name="pass" value="4367Xs" /><br />
+		<input type="submit" class="bouton" value="Connexion" />
 	</fieldset>
 </form>
